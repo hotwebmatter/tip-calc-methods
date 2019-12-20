46e3ee0e18bb2fc8caaf1f37e758a5b2c9b589ab
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-20 11:40:44 -0500

    Declare class-level constants

--- Program.cs
+++ Program.cs
@@ -5,9 +5,15 @@ namespace tip_calc_methods
 {
     class Program
     {
+        // declare class-level constants
+        const double TAX_PERCENT = .09,
+                     LOW_TIP_PERCENT = .15,
+                     HIGH_TIP_PERCENT = .2;
         static void Main(string[] args)
         {
-            WriteLine("Hello World!");
+            WriteLine("{0, 20} {1, 15:P0}", "Tax Percentage:", TAX_PERCENT);
+            WriteLine("{0, 20} {1, 15:P0}", "Low Tip Percentage:", LOW_TIP_PERCENT);
+            WriteLine("{0, 20} {1, 15:P0}", "High Tip Percentage:", HIGH_TIP_PERCENT);
         }
     }
 }
